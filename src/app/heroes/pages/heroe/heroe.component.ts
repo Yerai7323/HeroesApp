81ec6568be3dab4cffc53d9a343274b48b57d77e
Yerai Espinar Moreno <yespinar.moreno@atsistemas.com>
2022-02-04 12:45:39 +0100

    Updated heroeComponent

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { Heroe } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
+
+
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
   styleUrls: ['./heroe.component.css']
 })
 export class HeroeComponent implements OnInit {
+  
+  heroe!: Heroe;
 
-  constructor() { }
+  constructor(
+    private activatedroute: ActivatedRoute,
+    private heroesService: HeroesService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.activatedroute.params
+    .pipe(
+      switchMap( ({id}) => this.heroesService.getHeroesById(id) )
+    )
+    .subscribe( heroe => this.heroe = heroe );
   }
 
+  volverListado() {
+    this.router.navigate(['/heroes/listado']);
+  }
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -5,6 +5,7 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatListModule} from '@angular/material/list';
 import {MatCardModule} from '@angular/material/card';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 
 
@@ -17,6 +18,7 @@ import {MatCardModule} from '@angular/material/card';
     MatIconModule,
     MatListModule,
     MatCardModule,
+    MatProgressBarModule
   ],
 
 })
